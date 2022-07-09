7d37276ee27d721719b4f0b7ef3820c075575fa2
Dennis Akagha <d.akagha20@gmail.com>
2022-07-09 20:31:33 +0100

    Add validations and spec

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,6 +3,17 @@ class Post < ApplicationRecord
   has_many :likes, class_name: 'Like', foreign_key: 'post_id'
   has_many :comments, class_name: 'Comment', foreign_key: 'post_id'
 
+  # CommentsCounter must be an integer greater than or equal to zero.
+  validates :commentsCounter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+
+  # Title must not be blank. Title must not exceed 250 characters.
+  validates :title, length: { maximum: 250 }, presence: true
+
+  # LikesCounter must be an integer greater than or equal to zero.
+  validates :likesCounter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+
+  after_save :update_posts_counter
+
   # A method which returns the 5 most recent comments for a given post.
   def most_recent_comments
     comments.order(created_at: :desc).limit(5)
