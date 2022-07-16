c569ccf506613972cce3a9f46a2fd6e14ddc960e
Dennis Akagha <d.akagha20@gmail.com>
2022-07-16 04:15:58 +0100

    Add form

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -4,9 +4,28 @@ class PostsController < ApplicationController
     @post = Post.where(user_id: @user.id).order(created_at: :desc)
   end
 
+  def create
+    @post = current_user.posts.new(post_params)
+    if @post.save
+      redirect_to "/users/#{@post.user.id}/posts/#{@post.id}"
+    else
+      render :new
+    end
+  end
+  
+
+  def new
+    @post = Post.new
+  end
+
   def show
+    @current_user = current_user
     @user = User.find(params[:user_id])
     @post = Post.find(params[:id])
     @comment = Comment.where(post_id: @post.id).order(created_at: :desc)
+  end
+
+  def post_params
+    params.require(:post).permit(:title, :text)
   end
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -2,4 +2,14 @@ class Post < ApplicationRecord
   belongs_to :user
   has_many :comments, class_name: 'Comment', foreign_key: 'post_id'
   has_many :likes, class_name: 'Like', foreign_key: 'post_id'
+
+  after_save :update_posts_counter
+
+  def update_posts_counter
+    user.increment!(:posts_counter)
+  end
+
+  def recent_comments
+    comments.last(5)
+  end
 end
