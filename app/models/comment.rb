b3414b653c55696f7e061cbbf1b5678d24c0be80
Rao Muhammad Akif Tufail <akifrao@gmail.com>
2022-07-23 21:36:28 +0500

    Update comment.rb

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -10,6 +10,11 @@ class Comment < ApplicationRecord
   after_destroy :decrement_comments_counter
 
   private
+  def increment_comments_counter
+    post.increment!(:comments_counter)
+  end
 
-  
+  def decrement_comments_counter
+    post.decrement!(:comments_counter)
+  end  
 end
