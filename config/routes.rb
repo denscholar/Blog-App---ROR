a17f3afa652e0ff8465411369803a9cf3af7552b
raoakif <akifrao@gmail.com>
2022-07-24 01:58:43 +0500

    Add api-endpoints

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
-  post 'api/login', to: 'auth#login', default: {:format => :json}
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  post '/login', to: 'auth#login', default: {:format => :json}
+  post '/signup', to: 'auth#signup', default: {:format => :json}
+  post '/logout', to: 'auth#logout', default: {:format => :json}
 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   resources :users, only: %i[index show] do
@@ -14,4 +17,12 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   root "users#index"
+  
+  scope 'api' do
+    get '/posts/:id/comments' => 'api/comments#index', as: :api_post_comments
+    post '/posts/:id/comments' => 'api/comments#create', as: :api_post_comments_create
+    resources :users, only: [] do
+      get '/posts' => 'api/posts#index', as: :api_user_posts
+    end
+  end
 end
