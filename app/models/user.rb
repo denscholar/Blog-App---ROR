1d1d0a0f8d9bef1965fa01c1307fad0b64e85050
Dennis Akagha <d.akagha20@gmail.com>
2022-07-23 02:10:53 +0100

    Add role column to the user table

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,4 @@
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable,
          :confirmable, :trackable
